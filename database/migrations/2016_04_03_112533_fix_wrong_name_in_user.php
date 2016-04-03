9a875c3b7a7b4d5a193f6a241e27a8016999d538
magichan <you@example.com>
2016-04-03 19:30:01 +0800

    修复数据库变量名错误

---
+++ database/migrations/2016_04_03_112533_fix_wrong_name_in_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixWrongNameInUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropColumn('self_introudce');
+          $table->string('self_introduce');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
