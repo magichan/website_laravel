4a3f771f6d9fa7c027974e7f0a4a99c82374dcae
magichan <you@example.com>
2016-04-03 21:51:17 +0800

    将 html 文件放入到站中

---
+++ resources/views/about.blade.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+
+	<title>关于</title>
+
+	<link rel="shortcut icon" href="/assets/images/gt_favicon.png">
+
+	<link rel="stylesheet" media="screen" href="/assets/css/Opensans.css">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="/assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="/assets/css/main.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.html"><img src="/assets/images/logo.png" alt="Association of Network and Technology"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li><a href="index.html">主页</a></li>
+					<li><a href="http://blog.xiyounet.org">专栏</a></li>
+					<li><a href="http://lib.xiyounet.org">网协图书馆</a></li>
+					<li class="active"><a href="about.html">关于</a></li>
+					<li><a href="contact.html">联系我们</a></li>
+					<!-- <li><a class="btn" href="signin.html">SIGN IN / SIGN UP</a></li> -->
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+	<header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb">
+			<li><a href="index.html">主页</a></li>
+			<li class="active">关于</li>
+		</ol>
+
+
+		<div class="row">
+
+			<!-- Article main content -->
+			<article class="col-sm-8 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">About us</h1>
+				</header>
+				<h3>上古时期 <span><i class="fa fa-child fa-lg"></i></span></h3>
+				<p>网协 xiyou<abbr title="Association of Network and Technology">ant</abbr> （全称西邮计算机院网络科技协会），专注于为技术爱好者提供友好的环境，构建技术友好的大家庭。自 2010 年 9 月成立以来，已经吸引了众多技术爱好者以及各路牛人的加入。</p>
+				<h3>石器时代 <span><i class="fa fa-paw fa-lg"></i></span></h3>
+				<p>从 08 级学长学姐的手中延续如今，协会成员凭借自身不断进取的追求，以及对技术的热忱。多数成员最终都得以在百度、腾讯、阿里巴巴、网易、360 等知名互联网公司就职，在激烈的竞争中脱颖而出。</p>
+				<h3>蒸汽朋克 <span><i class="fa fa-hand-spock-o"></i></span></h3>
+				<p>2014 年网协大本营则转移至到了长安校区东区的 FZ130。从最初西邮长安校区西区２号实验楼的 414，到聚集着一群热爱技术，不断进取的成员的大家庭。我们一直都在努力…「创意杯」网页设计大赛也是延续下来的，在第二学期进行的「保留节目」。创意杯则是我们的一大特色，每次举办都会有新的收获。</p>
+				<p></p>
+				<h3>号角吹响 <span><i class="fa fa-paper-plane-o fa-lg"></i></span></h3>
+				<p>协会会在每学年的第一学期进行纳新，吸收新的成员,注入新的血液和活力。涵盖各个方向的开发需求,
+				为内部成员提供技术分享，团队合作，以及技术创新的平台，目前协会方向主要分为：</p>
+				<div class="bs-example" data-example-id="contextual-table">
+			     <table class="table">
+			      <thead>
+			        <tr>
+			          <th></th>
+			          <th>成员方向</th>
+			          <th>相关简介</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			        <tr class="success">
+			          <th scope="row"><i class="fa fa-chrome fa-lg"></i></th>
+			          <td>Web 前端类</td>
+			          <td>详细参考:<a href="http://www.zhihu.com/question/19568008">「如何面试前端工程师?」</a></td>
+			        </tr>
+			        <tr class="info">
+			          <th scope="row"><i class="fa fa-linux fa-lg"></i></th>
+			          <td>技术运维</td>
+			          <td>详细参考:<a href="http://www.zhihu.com/question/23665108">「运维工程师必须掌握的基础技能有哪些?」</a></td>
+			        </tr>
+			        <tr class="warning">
+			          <th scope="row"><i class="fa fa-cog fa-spin fa-lg"></i></th>
+			              <!-- char Data[] = {0x36,0x29,0x6D,0x68,0x6E,0x69,0x66,0x69,0x73,0x29,0x74,0x6E,0x69,0x66,0x66,0x77,0x77,0x29,0x64,0x68,0x6A};void main(){printf("Please Internet the web:\n");for(i=0;i<sizeof(Data);i++)printf("%c",Data[i]^7);} -->
+			          <td>信息安全</td>
+			          <td>广泛领域,分而治之,一般从学会使用搜索框开始</td>
+			        </tr>
+			        <tr class="danger">
+			          <th scope="row"><i class="fa fa-dribbble fa-lg"></i></th>
+			          <td>视觉设计</td>
+			          <td>Visual Design,UI 及交互设计师（interaction designer）<a href="http://www.zhihu.com/question/19582408">请立即撤离</a></td>
+			        </tr>
+			      </tbody>
+			    </table>
+			  </div>
+				<hr>
+
+				<div>
+
+
+				</div>
+			</article>
+			<!-- /Article -->
+
+			<!-- Sidebar -->
+			<aside class="col-sm-4 sidebar sidebar-right">
+
+				<div class="widget">
+					<img src="/assets/images/mac.jpg" alt="" class="pull-center img-rounded"  height="150" width="250" >
+					<h4><span><i class="fa fa-list-ul"></i></span> <a href="./member/">成员列表</a></h4>
+					<h4><span><i class="fa fa-download"></i></span> 团队成就(2012~2015)</h4>
+
+					<div class="bs-example" data-example-id="simple-ul">
+					    <ul>
+					      <li>2013 年 Cl0udShe1l 团队阿里巴巴全国大学生网络安全竞赛 36 名</li>
+					      <li>2015 扫黄大队 GCTF 20 名</li>
+					      <li><a href="http://is.campus.360.cn/">第二届 360 信息安全大赛</a>陕西赛区优胜团队 -- Cl0udShe1l</li>
+					      <li><a href="http://isg.campus.360.cn/">第三届 360 信息安全大赛</a> 37 名 -- All_4_win</li>
+					    </ul>
+					</div>
+
+					<h4><span><i class="fa fa-download"></i></span> 成员成就(2012~2014 届)</h4>
+
+					<div class="bs-example" data-example-id="simple-ul">
+					    <ul>
+					      <li>瞿祥轩 2012 年 ACM 省赛铜牌</li>
+					      <li>李昱辰 2013 年挑战杯三等奖</li>
+					      <li>谷仕涛 2014 年蓝桥杯三等奖</li>
+					      <li>陈涛德
+					        <ul>
+					          <li>2014 年全国大学生“美新杯”物联网竞赛国赛三等奖</li>
+					          <li>2014 年全国大学生“皮塞杯”信息技术应用竞赛国赛二等奖</li>
+					          <li>2015 年全国大学生“三创”电子商务竞赛省赛二等奖。</li>
+					        </ul>
+					      </li>
+					      <li>韩强 2014 年微软创新杯三等奖</li>
+					      <li>王凯 2015 年创新创意创业陕西二等奖</li>
+					    </ul>
+					</div>
+				</div>
+
+			</aside>
+			<!-- /Sidebar -->
+
+		</div>
+	</div>	<!-- /container -->
+
+
+
+	<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">联系我们</h3>
+						<div class="widget-body">
+							<address>陕西省西安市长安区西安邮电大学 东区FZ130	</address>
+								<p><b>邮箱:</b><a href="mailto:#">admin@xiyouant.org</a><br></p>
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Follow Us</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons">
+								<a class="social-btn bspopover" data-toggle="popover" data-placement="top" data-trigger="hover" data-html="true"data-content="<img src=/assets/images/wechat.jpg style='width: 140px; height: 140px;'>" data-original-title="" title="">
+      							<i class="fa fa-weixin fa-2"></i>
+    							</a>
+								<a href="https://github.com/xiyouant" target="_blank"><i class="fa fa-github fa-2"></i></a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">内容许可</h3>
+						<div class="widget-body">
+							<p>本站由 <a href="https://www.digitalocean.com/?refcode=3c8422b30f9d">DigitalOcean</a> 提供服务器支持 七牛 提供 CDN 存储服务</p>
+							<p>除特别说明外，用户内容均采用
+							<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/3.0/cn/">知识共享署名-相同方式共享 3.0 中国大陆许可协议</a>进行许可</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="simplenav">
+								<a href="#">主页</a> |
+								<a href="blog/">专栏</a> |
+								<a href="">网协图书馆</a> |
+								<a href="contact.html">联系我们</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="text-center">Copyright &copy; 2010-<span id="copyright"></span> 西安邮电大学计算机院网络科技协会.</p>
+							<script>var year = new Date().getFullYear();document.getElementById("copyright").innerHTML=year;</script>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+	</footer>
+
+
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://cdn.staticfile.org/jquery/2.1.1-rc2/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	<!--  popover initialization -->
+	<script>$(function () {$('[data-toggle="popover"]').popover()})</script>
+</body>
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<!-- <meta name="viewport"    content="width=device-width, initial-scale=1.0"> -->
+	<!-- Turn off scale on mobile device -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="description" content="">
+
+	<title>西安邮电大学计算机院网络科技协会</title>
+	<link rel="shortcut icon" href="/assets/images/gt_favicon.png">
+	<link rel="stylesheet" href="/assets/css/Opensans.css">
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="/assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="/assets/css/main.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body class="home">
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Association of Network and Technology"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">主页</a></li>
+					<li><a href="http://blog.xiyounet.org">专栏</a></li>
+					<li><a href="http://lib.xiyounet.org">网协图书馆</a></li>
+					<li><a href="about.html">关于</a></li>
+					<li><a href="contact.html">联系我们</a></li>
+					<li><a href="http://slide.xiyounet.org"><b>Slide</b></a></li>
+					<li><a class="btn" href="Countdown/">2015 纳新快速报名通道</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<h1 class="lead">IDEA, SHARE, BEYOND</h1>
+				<p class="tagline">专注于为技术爱好者提供友好的环境，构建技术友好的大家庭。<br>
+				从 0 到 1，为自己创造无限的机会与价值。</p>
+				<p><a class="btn btn-default" role="button" href="about.html">了解更多</a> <a class="btn btn-action" role="button" href="http://join.xiyounet.org" target="blank">加入我们</a></p>
+			</div>
+		</div>
+	</header>
+	<!-- /Header -->
+
+	<!-- Intro -->
+	<div class="container text-center">
+		<br> <br>
+		<h2 class="thin">最好的地方，来证明自己</h2>
+		<p class="text-muted">
+			自 2010 年 9 月成立以来，已经吸引了众多技术爱好者以及各路牛人的加入。<br>
+			从 0 到 1，为自己创造无限的机会与价值。网协 5 年,感谢所有并肩同行的人<br>
+			<b>下一位也许就是你</b>
+		</p>
+	</div>
+	<!-- /Intro-->
+
+	<!-- Highlights - jumbotron -->
+	<div class="jumbotron top-space">
+		<div class="container">
+
+			<h3 class="text-center thin">Let's start</h3>
+
+			<div class="row">
+				<div class="col-xs-12 col-md-3 col-sm-6  highlight">
+					<div class="h-caption">
+					<a href="http://open.xiyouant.org" target="_blank"><i class="fa fa-cogs fa-5"></i></a><h4>开放资源</h4>
+					</div>
+
+					<div class="h-body text-center">
+						<p>协会内部开放的电子书以及相关文档和报告资源</p>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-md-3 col-sm-6  highlight">
+					<div class="h-caption"><a href="http://doc.xiyounet.org/"><i class="fa fa-file fa-5"></i></a><h4>文档</h4></div>
+					<div class="h-body text-center">
+						<p>历届的相关管理文档以及相关规范</p>
+					</div>
+				</div>
+
+
+				<div class="col-xs-12 col-md-3 col-sm-6  highlight">
+					<div class="h-caption"><a href="https://github.com/xiyouant" target="_blank"><i class="fa fa-github fa-5"></i></a><h4>项目平台</h4>
+					</div>
+					<div class="h-body text-center">
+						<p>在这里你会看到,我们对外开放的相关项目,了解我们在做什么</p>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-md-3 col-sm-6  highlight">
+					<div class="h-caption"><a href="http://lib.xiyounet.org/stulogin.php"><i class="fa fa-smile-o fa-5"></i></a><h4>快速登入</h4></div>
+					<div class="h-body text-center">
+						<p>网协图书馆内部成员快速登入通道</p>
+					</div><!-- /Highlights -->
+				</div>
+
+			</div> <!-- /row  -->
+
+		</div>	<!-- container -->
+	</div>
+
+
+
+	<div class="container">
+
+		<h2 class="text-center top-space">Frequently Asked Questions</h2>
+		<br>
+
+		<div class="row">
+			<div class="col-sm-6">
+				<h3>协会内部成员的方向目前都有什么?</h3>
+				<p>协会目前的方向目前大致归为以下几类:<br><b>Web 「全端」、技术运维、 信息安全以及视觉设计</b></p>
+			</div>
+			<div class="col-sm-6">
+				<h3>你们管理模式是怎样的?</h3>
+				<p>我们不会一味标榜管理上如何如何宽松，毕竟任何一个方向都是有压力的，任务是需要及时交付的。成员之间相互合作或者是共同完成一项任务，是要相互满足期望值的，不合适的人是要离开的。除了一些<b>硬规定</b>之外，来协会的时间在合理范围内是弹性的，每个人对自己负责的工作是有话语权的，成员无论级别是相互尊重的，一切基于技术上的讨论都是平等的。管理方式上以让大家能更好地学习和进步为标准。</p>
+			</div>
+		</div> <!-- /row -->
+
+		<div class="row">
+				<div class="col-sm-6">
+					<h3>一些福利</h3>
+					<ul>
+						<li>偶尔会有小惊喜</li>
+						<br>
+							<li>不定期的安排内部的技术分享,以及户外活动进行团队建设。</li>
+		  				<br>
+		  				<li>不定期的组织和鼓励成员去参加其他组织的知识分享以及开源社区的相关活动。</li>
+		  				<br>
+		  				<li>我们为每一个成员提供<b>无需任何配置的全局无缝科学上网</b>和<b>国外站点加速</b>。<br>旨为从细节开始,
+						提高大家的效率和生产力。</li>
+					</ul>
+				</div>
+
+				<div class="col-sm-6">
+					<h3>文化及价值观</h3>
+					<ul>
+	  				<li><b>开放</b>:对内，我们确保每一个人都能了解我们做每件事的原因。以及整个协会内部的运作方式和相关机制；对外，我们尽量开放相关的资源、分享相关的知识。</li>
+	  				<li><b>平等和尊重</b>:我们确保每个人对自己负责的工作都有绝对的话语权，成员间彼此无论级别都应是相互尊重的。做为一个成熟的个体，保持对他人的尊重，也是成员间彼此共事的基本要求。</li>
+	  				<li><b>Be the One</b>:我们希望并鼓励每一个成员都能够独立思考,无论是在哪个组别、位兼何职都应该承担领袖的角色,发起和推动那些认为正确的事情，而不是随波逐流地盲从普遍做法。</li>
+					</ul>
+				</div>
+		</div> <!-- /row -->
+
+
+	</div><!-- /container -->
+
+	<!-- Social links. Ready-->
+	<section id="social">
+		<div class="container">
+			<div class="wrapper clearfix">
+			</div>
+		</div>
+	</section>
+	<!-- /social links -->
+
+
+	<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">联系我们</h3>
+						<div class="widget-body">
+							<address>陕西省西安市长安区西安邮电大学 东区 FZ130	</address>
+								<p><b>邮箱:</b><a href="mailto:admin@xiyouant.org">admin@xiyouant.org</a><br></p>
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Follow Us</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons">
+								<a class="social-btn bspopover" data-toggle="popover" data-placement="top" data-trigger="hover" data-html="true"data-content="<img src=assets/images/wechat.jpg style='width: 140px; height: 140px;'>" data-original-title="" title="">
+      							<i class="fa fa-weixin fa-2"></i>
+    							</a>
+								<a href="https://github.com/xiyouant" target="_blank"><i class="fa fa-github fa-2"></i></a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">内容许可</h3>
+						<div class="widget-body">
+							<p>本站由 <a href="https://www.digitalocean.com/?refcode=3c8422b30f9d">DigitalOcean</a> 提供服务器支持<a href="http://www.qiniu.com/"> 七牛 </a>提供 CDN 存储服务</p>
+							<p>除特别说明外，用户内容均采用
+							<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-sa/3.0/cn/">知识共享署名-相同方式共享 3.0 中国大陆许可协议</a>进行许可</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 col-sm-6 widget">
+						<div class="widget-body">
+<!-- 							<p class="simplenav">
+								<a href="#">主页</a> |
+								<a href="http://blog.xiyounet.org">专栏</a> |
+								<a href="http://lib.xiyounet.org">网协图书馆</a> |
+								<a href="contact.html">联系我们</a>
+							</p> -->
+							<p class="text-center">Copyright &copy; 2010-<span id="copyright"></span> 西安邮电大学计算机院网络科技协会.</p>
+							<script>var year = new Date().getFullYear();document.getElementById("copyright").innerHTML=year;</script>
+						</div>
+					</div>
+
+					<div class="col-md-6 col-sm-6 widget">
+						<div class="widget-body">
+							<p class="text-center">陕ICP备15013578号 当前呈现版本 15.11.4</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+	</footer>
+
+
+
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://cdn.staticfile.org/jquery/2.1.1-rc2/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	<!--  popover initialization -->
+	<script>$(function () {$('[data-toggle="popover"]').popover()})</script>
+	<!-- console_info -->
+	<script>window.console && window.console.info("喜欢看代码?或者是在找 Bug? 快来为我们添砖加瓦:\nhttp://join.xiyounet.org\n另外,随时欢迎小伙伴来参观和面基。地址：西安邮电大学长安校区，东区 FZ130");</script>
+	<!-- Google Analytics -->
+	<script src="assets/js/Google_Analytics.js"></script>
+    <!-- char Data[] = {0x36,0x29,0x6D,0x68,0x6E,0x69,0x66,0x69,0x73,0x29,0x74,0x6E,0x69,0x66,0x66,0x77,0x77,0x29,0x64,0x68,0x6A};void main(){printf("Please Internet the web:\n");for(i=0;i<sizeof(Data);i++)printf("%c",Data[i]^7);} -->
+
+</body>
+</html>
