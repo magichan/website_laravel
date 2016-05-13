c9a0584b7cbec85383f9e8bc50cf5269711fd6da
magichan <you@example.com>
2016-05-13 09:27:11 +0800

    url 切割的 测试

--- app/Http/Controllers/Test/TestAjax.php
+++ app/Http/Controllers/Test/TestAjax.php
@@ -10,13 +10,25 @@ use App\Http\Controllers\Controller;
 
 class TestAjax extends Controller
 {
+  private $constantPath = "?imageMogr2/crop/!";// 七牛处理图片的必要前缀
   public function  index()
   {
     return view('test.testajax');
   }
+  /* 输入 json 
+   *"sourceLink":图片的 URl,
+   "a_x": ,
+   "a_y": ,
+   "e_width": ,
+   "e_height": 
+   * */
   public function getpost(Request $request)
   { 
-    $portrait_url = $request->input('portrait_url');
+    $portrait_url = $request->input('sourceLink');
+    $a_x          = $request->input('a_x');
+    $a_y          = $request->input('a_y');
+    $e_width      = $request->input('e_width');
+    $e_height     = $request->input('e_height');
 
     if( isset($portrait_url) === FALSE )
     {
@@ -26,8 +38,22 @@ class TestAjax extends Controller
      
       return response()->json(['status'=>'error','reason'=>'the portrait_url value is not a url']);
     }else{
-      return response()->json(['status'=>'success']);
+     
+      $photo_url = $this->sliceUrlToGetHost($portrait_url).$this->sliceUrlToGetPath($portrait_url).$this->constantPath.$e_width.'x'.$e_height.'a'.$a_x.'a'.$a_y;//将 传入参数 拼接  成 符合特定格式的 url 
+      return response()->json(['status'=>$photo_url]); 
     }
+  }
+  /* 切割获取的 url ,将 host 部分取出  
+   * */
+  private function sliceUrlToGetHost($url)
+  {
+    $urlComponetArray = parse_url($url); // 返回 含有 协议， host, user, 等信息的数组 
+    $url = $urlComponetArray['scheme'].'://'.$urlComponetArray['host'];
+    return $url;
+  }
+  private function sliceUrlToGetPath($url)
+  {
+    return  parse_url($url,PHP_URL_PATH); // 返回 含有 协议， host, user, 等信息的数组 
   }
 }
 
