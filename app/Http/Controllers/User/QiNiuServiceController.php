39e2186d852d440dee287ec6b96729f9bc4ec758
magichan <you@example.com>
2016-05-14 17:26:00 +0800

    七牛 uptoken API

--- app/Http/Controllers/User/QiNiuServiceController.php
+++ app/Http/Controllers/User/QiNiuServiceController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Service\QiniuService;
 
 class QiNiuServiceController extends Controller
 {
-    //
+   private $qiniuService;
+  function __construct(QiniuService $qiniuService)
+  {
+    $this->qiniuService = $qiniuService;
+
+  }
+  /* 得到 写在 .env 里的 bucket 的 uptoken 
+   * */
+  function getUserPhotoBucketUpToken()
+  {
+    $json_value = array('uptoken'=>$this->qiniuService->getEnvBucketUpToken());
+
+    return response()->json($json_value);
+  }
+   //
 }
